67d2696287e16494fc5495c0b9c944efe9f809f2
spaofficial11-hash <spaofficial11@gmail.com>
2025-08-07 23:19:35 +0530

    Update api.js

--- admin-panel/src/services/api.js
+++ admin-panel/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://ah-game-backend-iezb.onrender.com/api',
 });
 
 // Add Authorization token for admin
